ebd1201a8bdbf3bf5473f8e04305e556b20bfe62
Elijah Sanders <esanders@cmsaero.net>
2023-03-26 19:07:22 -0500

    Converted GUI into class to improve state management.

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,104 +1,123 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import N,S,E,W
 from tkinter import ttk
 
-def test_fn(*args):
-    print("Button pressed.")
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        
+        self.title("Space Weather Analysis Tool")
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
 
-def validate_dyear(*args):
-    val = dyear_val.get()
-    if len(val) > 0:
-        lastkey = val[len(val) - 1]
-    else:
-        return
+        self.create_body_frame()
 
-    if not lastkey.isdigit():
-        dyear_input.delete(len(val) - 1, "end")
-    elif len(val) > 4:
-        dyear_input.delete(4, "end")
+    def create_body_frame(self):
+        self.body = ttk.Frame(self, padding="3 3 12 12")
 
-def clear_dyear(*args):
-    dyear_input.delete(0, "end")
+        # canvas initialization
+        self.h_scroll = ttk.Scrollbar(self, orient=tk.HORIZONTAL)
+        self.v_scroll = ttk.Scrollbar(self, orient=tk.VERTICAL)
+        self.grid_canvas = tk.Canvas(self, scrollregion=(0, 0, 800, 800), yscrollcommand=self.v_scroll.set, xscrollcommand=self.h_scroll.set, background="white")
+        self.h_scroll['command'] = self.grid_canvas.xview
+        self.v_scroll['command'] = self.grid_canvas.yview
 
-def place_bus(canvas, x_val, y_val, bus_num):
-    rect_id = canvas.create_rectangle((x_val, y_val, x_val + 20, y_val + 20), fill="green", tags=('palette', 'palettered'))
-    canvas.create_text(x_val + 10, y_val + 10, text='B1', anchor='center', font='TkMenuFont', fill='white')
-    return rect_id
+        # buttons
+        self.load_btn = ttk.Button(self.body, text="Load Grid File", command=self.test_fn)
+        self.play_btn = ttk.Button(self.body, text="Play/Pause Simulation", command=self.test_fn)
+        self.date_label = ttk.Label(self.body, text="Solar Storm Date:")
 
-root = Tk()
-root.title("Space Weather Analysis Tool")
+        # enter date
+        self.dmonth_val = tk.StringVar()
+        self.dmonth_input = ttk.Combobox(self.body, textvariable=self.dmonth_val, width=3, state="readonly")
+        self.slash1_label = ttk.Label(self.body, text="/")
+        self.dday_val = tk.StringVar()
+        self.dday_input = ttk.Combobox(self.body, textvariable=self.dday_val, width=3, state="readonly")
+        self.slash2_label = ttk.Label(self.body, text="/")
+        self.dyear_val = tk.StringVar()
+        self.dyear_input = ttk.Entry(self.body, textvariable=self.dyear_val, width=5)
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
+        # enter time
+        self.time_in_label = ttk.Label(self.body, text="Solar Storm Time:")
+        self.hour_val = tk.StringVar()
+        self.hour_input = ttk.Combobox(self.body, textvariable=self.hour_val, width=3, state="readonly")
+        self.minute_val = tk.StringVar()
+        self.minute_input = ttk.Combobox(self.body, textvariable=self.minute_val, width=3, state="readonly")
+        self.ampm_val = tk.StringVar()
+        self.ampm_input = ttk.Combobox(self.body, textvariable=self.ampm_val, width=3, state="readonly")
 
-# canvas initialization
-h = ttk.Scrollbar(root, orient=HORIZONTAL)
-v = ttk.Scrollbar(root, orient=VERTICAL)
-canvas = Canvas(root, scrollregion=(0, 0, 800, 800), yscrollcommand=v.set, xscrollcommand=h.set, background="white")
-h['command'] = canvas.xview
-v['command'] = canvas.yview
+        # simulation time
+        self.time_label = ttk.Label(self.body, text="Time In Simulation: XX:XX")
 
-# rest of initialization
-load_btn = ttk.Button(mainframe, text="Load Grid File", command=test_fn)
-play_btn = ttk.Button(mainframe, text="Play/Pause Simulation", command=test_fn)
-date_label = ttk.Label(mainframe, text="Solar Storm Date:")
-dmonth_val = StringVar()
-dmonth_input = ttk.Combobox(mainframe, textvariable=dmonth_val, width=3, state="readonly")
-slash1_label = ttk.Label(mainframe, text="/")
-dday_val = StringVar()
-dday_input = ttk.Combobox(mainframe, textvariable=dday_val, width=3, state="readonly")
-slash2_label = ttk.Label(mainframe, text="/")
-dyear_val = StringVar()
-dyear_input = ttk.Entry(mainframe, textvariable=dyear_val, width=5)
-time_in_label = ttk.Label(mainframe, text="Solar Storm Time:")
-hour_val = StringVar()
-hour_input = ttk.Combobox(mainframe, textvariable=hour_val, width=3, state="readonly")
-minute_val = StringVar()
-minute_input = ttk.Combobox(mainframe, textvariable=minute_val, width=3, state="readonly")
-ampm_val = StringVar()
-ampm_input = ttk.Combobox(mainframe, textvariable=ampm_val, width=3, state="readonly")
-time_label = ttk.Label(mainframe, text="Time In Simulation: XX:XX:XX")
+        # date input configuration
+        self.dmonth_input.set("01")
+        self.dmonth_input["values"] = list(map(lambda val : str(val).rjust(2, "0"), list(range(1,13))))
+        self.dday_input.set("01")
+        self.dday_input["values"] = list(map(lambda val : str(val).rjust(2, "0"), list(range(1,32))))
+        self.dyear_input.insert(0,"YYYY")
+        self.dyear_input.bind("<FocusIn>", self.clear_dyear)
+        self.dyear_input.bind("<KeyRelease>", self.validate_dyear)
 
-# date input configuration
-dmonth_input.set("01")
-dmonth_input["values"] = list(map(lambda val : str(val).rjust(2, "0"), list(range(1,13))))
-dday_input.set("01")
-dday_input["values"] = list(map(lambda val : str(val).rjust(2, "0"), list(range(1,32))))
-dyear_input.insert(0,"YYYY")
-dyear_input.bind("<FocusIn>", clear_dyear)
-dyear_input.bind("<KeyRelease>",validate_dyear)
-hour_input.set("01")
-hour_input["values"] = list(map(lambda val : str(val).rjust(2, "0"), list(range(1,13))))
-minute_input.set("01")
-minute_input["values"] = list(map(lambda val : str(val).rjust(2, "0"), list(range(1,60))))
-ampm_input.set("AM")
-ampm_input["values"] = {"AM","PM"}
+        # time input configuration
+        self.hour_input.set("01")
+        self.hour_input["values"] = list(map(lambda val : str(val).rjust(2, "0"), list(range(1,13))))
+        self.minute_input.set("01")
+        self.minute_input["values"] = list(map(lambda val : str(val).rjust(2, "0"), list(range(1,60))))
+        self.ampm_input.set("AM")
+        self.ampm_input["values"] = ["AM", "PM"]
 
-# widget placement
-mainframe.grid(column=0, row=0, sticky=(N,W,E,S))
-h.grid(column=0, row=2, columnspan=13, sticky=(W,S,E))
-v.grid(column=13,row=1, rowspan=2, sticky=(N,S,E))
-canvas.grid(column=0, row=1, columnspan=5, sticky=(N,W,E,S))
-load_btn.grid(column=0, row=0, sticky=(N,W))
-play_btn.grid(column=1, row=0, sticky=(N,W))
-date_label.grid(column=2, row=0, sticky=(W,E), padx=5)
-dmonth_input.grid(column=3, row=0, sticky=(W,E))
-slash1_label.grid(column=4, row=0, sticky=(W,E))
-dday_input.grid(column=5, row=0, sticky=(W,E))
-slash2_label.grid(column=6, row=0, sticky=(W,E))
-dyear_input.grid(column=7, row=0, sticky=(W,E))
-time_in_label.grid(column=8, row=0, sticky=(W,E), padx=5)
-hour_input.grid(column=9, row=0, sticky=(W,E))
-minute_input.grid(column=10, row=0, sticky=(W,E))
-ampm_input.grid(column=11, row=0, sticky=(W,E))
-time_label.grid(column=12, row=0, sticky=(W,E), padx=5)
+        # widget placement
+        self.body.grid(column=0, row=0, sticky=(N,W,E,S))
 
-id = canvas.create_rectangle((10, 10, 30, 30), fill="red", tags=('palette', 'palettered'))
-#id2 = canvas.create_rectangle((1750, 1750, 1770, 1770), fill="red", tags=('palette', 'palettered'))
-id3 = place_bus(canvas, 700, 700, 2)
-#canvas.create_text(710, 710, text='B1', anchor='center', font='TkMenuFont', fill='white')
+        self.h_scroll.grid(column=0, row=2, columnspan=13, sticky=(W,S,E))
+        self.v_scroll.grid(column=13,row=1, rowspan=2, sticky=(N,S,E))
+        self.grid_canvas.grid(column=0, row=1, columnspan=5, sticky=(N,W,E,S))
 
-root.columnconfigure(0, weight=1)
-root.rowconfigure(1, weight=1)
+        self.load_btn.grid(column=0, row=0, sticky=(N,W))
+        self.play_btn.grid(column=1, row=0, sticky=(N,W))
+        
+        self.date_label.grid(column=2, row=0, sticky=(W,E), padx=5)
+        self.dmonth_input.grid(column=3, row=0, sticky=(W,E))
+        self.slash1_label.grid(column=4, row=0, sticky=(W,E))
+        self.dday_input.grid(column=5, row=0, sticky=(W,E))
+        self.slash2_label.grid(column=6, row=0, sticky=(W,E))
+        self.dyear_input.grid(column=7, row=0, sticky=(W,E))
 
-#root.bind("<Return>", test_fn)
+        self.time_in_label.grid(column=8, row=0, sticky=(W,E), padx=5)
+        self.hour_input.grid(column=9, row=0, sticky=(W,E))
+        self.minute_input.grid(column=10, row=0, sticky=(W,E))
+        self.ampm_input.grid(column=11, row=0, sticky=(W,E))
+        self.time_label.grid(column=12, row=0, sticky=(W,E), padx=5)
 
-root.mainloop()
+        # test code
+        self.place_bus(10, 10, 1)
+        self.place_bus(400,400, 2)
+        self.place_bus(500, 10, 3)
+        self.place_bus(700, 700, 4)
+
+    def test_fn(self, *args):
+        print("Button pressed.")
+
+    def validate_dyear(self, *args):
+        val = self.dyear_val.get()
+        if len(val) > 0:
+            lastkey = val[len(val) - 1]
+        else:
+            return
+
+        if not lastkey.isdigit():
+            self.dyear_input.delete(len(val) - 1, "end")
+        elif len(val) > 4:
+            self.dyear_input.delete(4, "end")
+
+    def clear_dyear(self, *args):
+        self.dyear_input.delete(0, "end")
+
+    def place_bus(self, x_val, y_val, bus_num):
+        rect_id = self.grid_canvas.create_rectangle((x_val, y_val, x_val + 20, y_val + 20), fill="green", tags=('palette', 'palettered'))
+        self.grid_canvas.create_text(x_val + 10, y_val + 10, text='B' + str(bus_num), anchor='center', font='TkMenuFont', fill='white')
+        return rect_id
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
